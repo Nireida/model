6cdee760632f3798eeeffbed7a9027325665b702
Gyulamirova.N <388771@mail.ru>
2020-05-19 20:40:39 +0300

    del zorro tabs

--- src/app/components/components/open-close/open-close.component.ts
+++ src/app/components/components/open-close/open-close.component.ts
@@ -7,17 +7,14 @@ import { Component } from '@angular/core';
 })
 export class OpenCloseComponent {
   public sideBar;
-  siderData = [{ name: 'comments', icon: 'idcard', title: 'Комментарии' },
-    { name: 'files', icon: 'folder', title: 'Файлы' },
-    { name: 'comments', icon: 'idcard', title: 'Комментарии' },
-    { name: 'files', icon: 'folder', title: 'Файлы' },
-    { name: 'files', icon: 'folder', title: 'Файлы' },
-    { name: 'comments', icon: 'idcard', title: 'Комментарии' },
-    { name: 'files', icon: 'folder', title: 'Файлы' },
-    { name: 'files', icon: 'folder', title: 'Файлы' },
-    { name: 'comments', icon: 'idcard', title: 'Комментарии' },
-    { name: 'files', icon: 'folder', title: 'Файлы' },
-    { name: 'history', icon: 'profile', title: 'История' }];
+  siderData = [{ name: '0', icon: 'idcard', title: 'Комментарии' },
+    { name: '1', icon: 'folder', title: 'Файлы' },
+    { name: '2', icon: 'idcard', title: 'Комментарии' },
+    { name: '3', icon: 'folder', title: 'Файлы' },
+    { name: '4', icon: 'folder', title: 'Файлы' },
+    { name: '5', icon: 'idcard', title: 'Комментарии' },
+    { name: '6', icon: 'folder', title: 'Файлы' },
+    { name: '7', icon: 'folder', title: 'Файлы' }];
 
 
   getSideBarValue(event) {
